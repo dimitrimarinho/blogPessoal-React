ec3b804f32c133c31e6fc3cb774a3d1f05547de8
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-06-28 13:46:06 -0300

    Criando componentes NavBar

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,23 +1,10 @@
 import React from "react";
-import {Box} from '@mui/material';
-import {Button, Paper} from '@material-ui/core';
 import './Home.css';
 
 function Home(){
     return(
         <>
-            <Paper>
-                <Box p={2}>
-                    <Box display="flex" justifyContent="center">
-                        <h1>Título</h1>
-                    </Box>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQiN3TbqjsJ_n3aCMSV0eM5ckoYXHv4Tmzng&usqp=CAU" alt="" style={{ width: "100%", height: "100%" }} />
-                    <Box display="flex" justifyContent="center" p={2}>
-                        <Button variant="contained" color="primary">Texto 1</Button>
-                        <Button variant="contained" color="secondary">Texto 2</Button>
-                    </Box>
-                </Box>
-            </Paper>
+          <h1>Home</h1>
         </>        
     );
 }
