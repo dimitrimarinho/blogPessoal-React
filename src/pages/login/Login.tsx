458ebf2e8a171c592acf0ebb80f2817c5256a4c0
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-07-01 16:04:06 -0300

    Comunicação com back-end - Login-Componente

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,54 +1,63 @@
-import React from "react";
-// import React, { ChangeEvent, useState } from "react";
+import React, { useState, ChangeEvent } from "react";
 import { Grid, Typography, TextField, Button } from "@material-ui/core";
 import { Box } from "@mui/material"
 import { Link } from 'react-router-dom';
+import UserLogin from "../../models/UserLogin";
 // import { Link, Navigate, useNavigate } from 'react-router-dom';
 import "./Login.css";
-// import UserLogin from '../../model/UserLogin';
 
 function Login() {
+
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        }
+    )
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value
+        })
+
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        console.log('userLogin: ' + userLogin);
+        // try {
+        //     const resposta = await api.post('/usuarios/logar', userLogin)
+        //     alert('Usuário Logado com sucesso!')
+        //     navigate('/home')
+        // } catch (error) {
+        //     alert('Informações inconsistentes ao logar')
+        // }
+    }
+
     // let navigate = useNavigate();   
     // const [getEmail, setEmail] = useState('Lucas@email.com');
-    // const [userLogin, setUserLogin] = useState<UserLogin>(
-    //     {
-    //         id: 0,
-    //         usuario: '',
-    //         senha: '',
-    //         token: ''
-    //     }
-    // )
 
     // function modificaEmail() {
     //     setEmail('Admin@email.com')
     // }
 
-    // function updatedModel(e: ChangeEvent<HTMLInputElement) {
-    //     setUserLogin({
-    //         ...UserLogin,
-    //         [e.target.name]: e.target.value
-    //     })
-    // }
 
-    // async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
-    //     e.preventDefault();
-    //     try{
-    //         const resposta = await api.post('/usuarios/logar', userLogin)
-    //         alert('Usuário Logado com sucesso!')
-    //         navigate('/home')
-    //     } catch(error){
-    //         alert('Informações inconsistentes ao logar')
-    //     }
-    // }
+
+
 
     return (
         <Grid container direction='row' justifyContent="center" alignItems="center">
             <Grid alignItems="center" xs={6}>
                 <Box paddingX={20}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="textos1">Entrar</Typography>
-                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
                             <Link to='/home' className='text-decorator-none'>
                                 <Button type='submit' variant='contained' color='primary'>
