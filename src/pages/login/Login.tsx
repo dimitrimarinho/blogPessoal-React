4438cbe82a140e46124b75e649a3fb8eed287da9
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-07-01 10:29:35 -0300

    Correções anteriores e Live Code 01-07-2022

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { Grid, Typography, TextField, Button } from "@material-ui/core";
 import { Box } from "@mui/material"
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import "./Login.css";
+import UserLogin from '../../model/UserLogin';
 
 function Login() {
+    let navigate = useNavigate();   
+    const [getEmail, setEmail] = useState('Lucas@email.com');
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        }
+    )
+
+    // function modificaEmail() {
+    //     setEmail('Admin@email.com')
+    // }
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement) {
+        setUserLogin({
+            ...UserLogin,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+        try{
+            const resposta = await api.post('/usuarios/logar', userLogin)
+            alert('Usuário Logado com sucesso!')
+            navigate('/home')
+        } catch(error){
+            alert('Informações inconsistentes ao logar')
+        }
+    }
+
     return (
         <Grid container direction='row' justifyContent="center" alignItems="center">
             <Grid alignItems="center" xs={6}>
