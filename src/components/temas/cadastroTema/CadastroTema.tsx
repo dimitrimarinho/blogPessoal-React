1e63230c90c3ab97709a6a78fda3fa8d78dbd4b6
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-07-07 14:46:26 -0300

    Post, Put e Tema

--- src/components/temas/cadastroTema/CadastroTema.tsx
+++ src/components/temas/cadastroTema/CadastroTema.tsx
@@ -1,13 +1,84 @@
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { Container, Typography, TextField, Button } from "@material-ui/core";
+import { useNavigate, useParams } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
+import Tema from "../../../models/Tema";
+import { buscaId, post, put } from "../../../services/Service";
 import './CadastroTema.css';
 
 function CadastroTema() {
+
+    let navigate = useNavigate();
+    const { id } = useParams<{id: string}>();
+    // eslint-disable-next-line
+    const [token, setToken] = useLocalStorage('token');
+    const [tema, setTema] = useState<Tema>({
+        id: 0,
+        descricao: ''
+    })
+
+    useEffect(() => {
+        if(token === ''){
+            alert("Você precisa estar logado")
+            navigate("/login")
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [token])
+
+    useEffect(() => {
+        if(id !== undefined){
+            findByID(id)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [id])
+
+    async function findByID(id: string){
+        buscaId(`/tema/${id}`, setTema, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    function updatedTema(e: ChangeEvent<HTMLInputElement>) {
+        setTema({
+            ...tema,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+        console.log("tema " + JSON.stringify(tema))
+        
+        if(id !== undefined) {
+            console.log(tema)
+            put(`/tema`, tema, setTema, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            alert('Tema atualizado com sucesso');
+        } else {
+            post(`/tema`, tema, setTema, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            alert('Tema cadastrado com sucesso')
+        }
+        back()        
+    }
+
+    function back() {
+        navigate("/temas")
+    }
+
     return (
         <Container maxWidth="sm" className="topo">
-            <form>
+            <form onSubmit={onSubmit}>
                 <Typography variant="h3" color="textSecondary" component="h1" align="center">Formulário de cadastro tema</Typography>
-                <TextField value="" id="descricao" label="descricao" variant="outlined" name="descricao" margin="normal" fullWidth />
+                <TextField value={tema.descricao} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedTema(e)} id="descricao" label="descricao" variant="outlined" name="descricao" margin="normal" fullWidth />
                 <Button type="submit" variant="contained" color="primary">
                     Finalizar
                 </Button>
